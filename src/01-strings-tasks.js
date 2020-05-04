f98666ee9c222b6e3797d814ce8fd6dd01069057
svdfsdev <58512937+svdfsdev@users.noreply.github.com>
2020-05-04 13:26:32 +0300

    Update 01-strings-tasks.js

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -282,8 +282,8 @@ function encodeToRot13(str) {
  *   isString('test') => true
  *   isString(new String('test')) => true
  */
-function isString(/* value */) {
-  throw new Error('Not implemented');
+function isString(value) {
+  return !!((typeof String(value) === 'string' && value !== undefined && value !== null && !Array.isArray(value)));
 }
 
 
