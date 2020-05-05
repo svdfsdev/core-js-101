29b040ff8019b3e7bcc6094b6ba132c58eda9c01
svdfsdev <58512937+svdfsdev@users.noreply.github.com>
2020-05-05 14:13:26 +0300

    Update 03-arrays-tasks.js

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -271,8 +271,17 @@ function getSecondItems(arr) {
  *  [ 'a', 'b', 'c', null ] => [ 'a', 'b','b', 'c','c','c',  null,null,null,null ]
  *  [ 1,2,3,4,5 ] => [ 1, 2,2, 3,3,3, 4,4,4,4, 5,5,5,5,5 ]
  */
-function propagateItemsByPositionIndex(/* arr */) {
-  throw new Error('Not implemented');
+function propagateItemsByPositionIndex(arr) {
+  function duplicate(el, index) {
+    const temp = [];
+
+    temp.length = index + 1;
+    temp.fill(el, 0);
+
+    return temp;
+  }
+
+  return arr.reduce((result, el, index) => result.concat(duplicate(el, index)), []);
 }
 
 
