5bc067ece2678b26593a2e57c5ef9302cd083070
svdfsdev <58512937+svdfsdev@users.noreply.github.com>
2020-05-11 00:05:40 +0300

    Update 08-conditions-n-loops-tasks.js

--- src/08-conditions-n-loops-tasks.js
+++ src/08-conditions-n-loops-tasks.js
@@ -282,8 +282,20 @@ function reverseInteger(num) {
  *   5436468789016589 => false
  *   4916123456789012 => false
  */
-function isCreditCardNumber(/* ccn */) {
-  throw new Error('Not implemented');
+function isCreditCardNumber(ccn) {
+  const temp = Array.from(String(ccn));
+
+  const start = (temp.length % 2 === 0) ? 0 : 1;
+  let sum = (temp.length % 2 === 0) ? 0 : +temp[0];
+
+  for (let i = start; i < temp.length; i += 2) {
+    const mult = +temp[i] * 2;
+
+    temp[i] = (mult > 9) ? mult - 9 : mult;
+
+    sum += (+temp[i] + +temp[i + 1]);
+  }
+  return sum % 10 === 0;
 }
 
 /**
