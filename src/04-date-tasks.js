0ce53b3a0bcf2d995c6714cca76714e6b7c40f3f
svdfsdev <58512937+svdfsdev@users.noreply.github.com>
2020-05-17 20:05:47 +0300

    Update 04-date-tasks.js

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -53,8 +53,10 @@ function parseDataFromIso8601(value) {
  *    Date(2012,1,1)    => true
  *    Date(2015,1,1)    => false
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  const testDate = new Date(date.getFullYear(), 1, 29);
+
+  return testDate.getDate() === 29;
 }
 
 
